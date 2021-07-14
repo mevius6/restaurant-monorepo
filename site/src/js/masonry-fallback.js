cef56ab5458a6d49c9177bfaebe5fe55822329a4
Nicolai Ivanov <iplexus@icloud.com>
2021-07-14 17:56:06 +0300

    upd vars naming

--- site/src/js/masonry-fallback.js
+++ site/src/js/masonry-fallback.js
@@ -1,44 +1,40 @@
+// https://css-tricks.com/a-lightweight-masonry-solution/
+
 let grids = [...document.querySelectorAll('.grid--masonry')];
 
-if(grids.length && getComputedStyle(grids[0]).gridTemplateRows !== 'masonry') {
-  grids = grids.map(grid => ({
-    _el: grid,
+if (grids.length && getComputedStyle(grids[0]).gridTemplateRows !== 'masonry') {
+  grids = grids.map((grid) => ({
+    elem: grid,
+    items: [...grid.childNodes].filter(
+      (c) => c.nodeType === 1 && +getComputedStyle(c).gridColumnEnd !== -1
+    ),
+    columnCount: 0,
     gap: parseFloat(getComputedStyle(grid).gridRowGap),
-    items: [...grid.childNodes].filter(c => c.nodeType === 1 && +getComputedStyle(c).gridColumnEnd !== -1),
-    ncol: 0
   }));
 
   const layout = () => {
-    grids.forEach(grid => {
-      // get the post relayout number of columns
-      let ncol = getComputedStyle(grid._el).gridTemplateColumns.split(' ').length;
+    grids.forEach((grid) => {
+      let columnRecount = getComputedStyle(grid.elem).gridTemplateColumns.split(' ').length;
 
-      // if the number of columns has changed
-      if(grid.ncol !== ncol) {
-        // update number of columns
-        grid.ncol = ncol;
+      if (grid.columnCount !== columnRecount) {
+        grid.columnCount = columnRecount;
 
-        // revert to initial positioning, no margin
-        grid.items.forEach(c => c.style.removeProperty('margin-top'));
+        grid.items.forEach((c) => c.style.removeProperty('margin-top'));
 
-        // if we have more than one column
-        if(grid.ncol > 1) {
-          grid.items.slice(ncol).forEach((c, i) => {
-            // bottom edge of item above
-            let prev_fin = grid.items[i].getBoundingClientRect().bottom,
-                // top edge of current item
-                curr_ini = c.getBoundingClientRect().top;
+        if (grid.columnCount > 1) {
+          grid.items.slice(columnRecount).forEach((c, i) => {
+            let prevItemBtm = grid.items[i].getBoundingClientRect().bottom,
+                currItemTop = c.getBoundingClientRect().top;
 
-            c.style.marginTop = `${prev_fin + grid.gap - curr_ini}px`;
+            c.style.marginTop = `${prevItemBtm + grid.gap - currItemTop}px`;
           });
         }
       }
     });
   }
 
-
-  addEventListener('load', () => {
+  window.addEventListener('load', () => {
     layout();
-    addEventListener('resize', layout, false);
+    window.addEventListener('resize', layout, false);
   }, false);
 }
