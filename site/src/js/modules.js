624e335db1e74e4d32cfff4d5b2d672fe1eab14c
Nicolai Ivanov <iplexus@icloud.com>
2021-07-24 03:57:53 +0300

    add module load

--- site/src/js/modules.js
+++ site/src/js/modules.js
@@ -1,13 +1,12 @@
 const parsedUrl = new URL(window.location.href);
 const doc = document,
       root = doc.documentElement;
+const PATH = './modules';
 
 // https://github.com/tc39/proposal-dynamic-import
-// https://github.com/parcel-bundler/parcel/issues/1401
-
 /* eslint-disable no-unused-vars */
 (async () => {
-  const toggle = await import('./modules/theme-switcher').then(() => {
+  const toggle = await import(`${PATH}/theme-switcher`).then(() => {
     const themeSwitch = doc.querySelector('theme-switch');
     root.setAttribute('data-theme-style', themeSwitch.mode === 'dark'
       ? 'dark'
@@ -22,10 +21,10 @@ const doc = document,
     parsedUrl.pathname === '/' ||
     parsedUrl.pathname === '/index.html'
   ) {
-    const hero = await import('./modules/hero');
-    const reveal = await import('./modules/reveal-effect');
-    const parallax = await import('./modules/parallax');
-    const map = await import('./modules/map');
+    const hero = await import(`${PATH}/hero`);
+    const reveal = await import(`${PATH}/reveal-effect`);
+    const parallax = await import(`${PATH}/parallax`);
+    const map = await import(`${PATH}/map`);
 
     loadReviews().then(() => loadCarousel());
     loadCardFeed();
@@ -34,27 +33,27 @@ const doc = document,
     parsedUrl.pathname === '/about' ||
     parsedUrl.pathname === '/about.html'
   ) {
-    const slideshow = await import('./modules/slideshow');
+    const slideshow = await import(`${PATH}/slideshow`);
   }
   if (
     parsedUrl.pathname === '/menu' ||
     parsedUrl.pathname === '/menu.html'
   ) {
-    const slideshow = await import('./modules/slideshow');
-    const tabs = await import('./modules/tabs');
-    const pdf = await import('./modules/pdf-viewer');
+    const slideshow = await import(`${PATH}/slideshow`);
+    const tabs = await import(`${PATH}/tabs`);
+    const pdf = await import(`${PATH}/pdf-viewer`);
   }
   if (
     parsedUrl.pathname === '/atm' ||
     parsedUrl.pathname === '/atm.html'
   ) {
-    const panorama = await import('./modules/panorama');
+    const pano = await import(`${PATH}/panorama`);
   }
   if (
     parsedUrl.pathname === '/gallery' ||
     parsedUrl.pathname === '/gallery.html'
   ) {
-    const slideshow = await import('./modules/slideshow');
+    const slideshow = await import(`${PATH}/slideshow`);
   }
 
   loadNav();
@@ -62,13 +61,13 @@ const doc = document,
 /* eslint-enable no-unused-vars */
 
 async function loadNav() {
-  const { default: DisclosureForNav } = await import('./modules/nav');
+  const { default: DisclosureForNav } = await import(`${PATH}/nav`);
   // eslint-disable-next-line no-unused-vars
   const navBtn = new DisclosureForNav(doc.querySelector('.nav-button'));
 }
 
 async function loadReviews() {
-  const { default: Reviews } = await import('./modules/reviews');
+  const { default: Reviews } = await import(`${PATH}/reviews`);
 
   const scrollRoot = doc.querySelector('[data-id="reviews"]');
   // eslint-disable-next-line no-undef
@@ -92,7 +91,7 @@ async function loadReviews() {
 }
 
 async function loadCardFeed() {
-  const { default: CardFeed } = await import('./modules/card-feed');
+  const { default: CardFeed } = await import(`${PATH}/card-feed`);
 
   const scrollRoot = doc.querySelector('[data-id="posts"]');
   // eslint-disable-next-line no-undef
@@ -118,5 +117,5 @@ async function loadCardFeed() {
 
 async function loadCarousel() {
   // eslint-disable-next-line no-unused-vars
-  const carousel = await import('./modules/carousel');
+  const carousel = await import(`${PATH}/carousel`);
 }
